814a118901dc642c2863104b4e3a3be64006967f
rotvince <ngeno2001@gmail.com>
2024-11-27 10:43:58 +0300

    Add addEventListerner  and Keyframes to modal

---
+++ DOM2 project2/modal.js
@@ -0,0 +1,17 @@
+let openBtn = document.querySelector('#open-btn');
+let modalContainer = document.querySelector('#modal-container');
+let closeBtn = document.querySelector('#close-btn');
+
+openBtn.addEventListener('click', function(){
+    modalContainer.style.display = 'block';
+})
+
+closeBtn.addEventListener('click', function(){
+    modalContainer.style.display = 'none';
+});
+
+window.addEventListener('click', function(e){
+    if (e.target === modalContainer){
+        modalContainer.style.display = 'none';
+    }
+});
